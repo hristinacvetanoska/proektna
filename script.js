f4efea7d9f3777ede6a3be4ccfa0c970e1041628
hristinacvetanoska <hristina2401@yahoo.com>
2020-12-16 15:00:46 +0100

    update

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function realtimeClock(){
     var rtClock = new Date();
     
     var hours = rtClock.getHours();
-    var minutes = rtClock.getMinutes();
-    var seconds = rtClock.getSeconds();
+    var min = rtClock.getMinutes();
+    var sec = rtClock.getSeconds();
   
     var amPm = ( hours < 12 ) ? "AM" : "PM";
   
     hours=(hours > 12) ? hours - 12 : hours;
   
     hours = ("0" + hours).slice(-2);
-    minutes = ("0" + minutes).slice(-2);
-    seconds = ("0" + seconds).slice(-2);
+    min = ("0" + min).slice(-2);
+    sec = ("0" + sec).slice(-2);
   
-    document.getElementById('clock').innerHTML = hours + " : " + minutes + " : " + seconds + " " + amPm;
+    document.getElementById('clock').innerHTML = hours + " : " + min + " : " + sec + " " + amPm;
     var t = setTimeout(realtimeClock, 500);
   }
